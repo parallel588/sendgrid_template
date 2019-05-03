51aeee4508acc8bf4d3ac64d22ae13910a1f0baf
Maksim Pechnikov <parallel588@gmail.com>
2019-05-03 14:05:18 +0300

    update gems

--- lib/sendgrid_template/version.rb
+++ lib/sendgrid_template/version.rb
@@ -1,5 +1,5 @@
 module SendgridTemplate
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 
   class Version < Struct.new(:id, :template_id,
                              :active, :name, :subject, :updated_at,
